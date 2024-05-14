e407242ffd599dc490aced52b054605191916991
DanielaCardo <daniela.cardonap1@udea.edu.co>
2024-05-13 20:17:58 -0500

    Formulario Orientacion Mayo 13

---
+++ Exámen Mental/Orientacion.Designer.cs
@@ -0,0 +1,492 @@
+﻿namespace Exámen_Mental
+{
+    partial class Orientacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gbOrientacion = new GroupBox();
+            lblNro = new Label();
+            lblPuntajeAcumulado = new Label();
+            lblOrientacion = new Label();
+            lblPuntuación = new Label();
+            gbOrientacion2 = new GroupBox();
+            lbl1Hasta5 = new Label();
+            lbl1 = new Label();
+            lbl1Año = new Label();
+            lbl2Mes = new Label();
+            lbl3Dia = new Label();
+            lbl4FechaHoy = new Label();
+            lbl5Hora = new Label();
+            lbl1DigaEnQue = new Label();
+            cbo1Punt1 = new ComboBox();
+            cbo1Punt2 = new ComboBox();
+            cbo1Punt3 = new ComboBox();
+            cbo1Punt4 = new ComboBox();
+            cbo1Punt5 = new ComboBox();
+            groupBox1 = new GroupBox();
+            cbo2Punt5 = new ComboBox();
+            cbo2Punt4 = new ComboBox();
+            cbo2Punt3 = new ComboBox();
+            cbo2Punt2 = new ComboBox();
+            lbl2DigaEnQue = new Label();
+            lblPisoBarrioVereda = new Label();
+            lblSitioLugar = new Label();
+            lbl2Departamento = new Label();
+            lblCiudad = new Label();
+            lblPais = new Label();
+            lbl2 = new Label();
+            lbl2Hasta5 = new Label();
+            cbo2Punt1 = new ComboBox();
+            btnOrientaCancelar = new Button();
+            btnOrientaGuardar = new Button();
+            gbOrientacion.SuspendLayout();
+            gbOrientacion2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gbOrientacion
+            // 
+            gbOrientacion.BackColor = SystemColors.ActiveCaption;
+            gbOrientacion.Controls.Add(lblNro);
+            gbOrientacion.Controls.Add(lblPuntuación);
+            gbOrientacion.Controls.Add(lblOrientacion);
+            gbOrientacion.Controls.Add(lblPuntajeAcumulado);
+            gbOrientacion.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            gbOrientacion.Location = new Point(40, 28);
+            gbOrientacion.Name = "gbOrientacion";
+            gbOrientacion.Size = new Size(713, 44);
+            gbOrientacion.TabIndex = 0;
+            gbOrientacion.TabStop = false;
+            // 
+            // lblNro
+            // 
+            lblNro.AutoSize = true;
+            lblNro.Location = new Point(18, 19);
+            lblNro.Name = "lblNro";
+            lblNro.Size = new Size(30, 17);
+            lblNro.TabIndex = 0;
+            lblNro.Text = "No.";
+            // 
+            // lblPuntajeAcumulado
+            // 
+            lblPuntajeAcumulado.AutoSize = true;
+            lblPuntajeAcumulado.Location = new Point(80, 19);
+            lblPuntajeAcumulado.Name = "lblPuntajeAcumulado";
+            lblPuntajeAcumulado.Size = new Size(129, 17);
+            lblPuntajeAcumulado.TabIndex = 1;
+            lblPuntajeAcumulado.Text = "Puntaje Acumulado";
+            lblPuntajeAcumulado.Click += this.label1_Click;
+            // 
+            // lblOrientacion
+            // 
+            lblOrientacion.AutoSize = true;
+            lblOrientacion.Location = new Point(351, 19);
+            lblOrientacion.Name = "lblOrientacion";
+            lblOrientacion.Size = new Size(80, 17);
+            lblOrientacion.TabIndex = 2;
+            lblOrientacion.Text = "Orientación";
+            // 
+            // lblPuntuación
+            // 
+            lblPuntuación.AutoSize = true;
+            lblPuntuación.Location = new Point(613, 19);
+            lblPuntuación.Name = "lblPuntuación";
+            lblPuntuación.Size = new Size(78, 17);
+            lblPuntuación.TabIndex = 3;
+            lblPuntuación.Text = "Puntuación";
+            // 
+            // gbOrientacion2
+            // 
+            gbOrientacion2.BackColor = SystemColors.GradientInactiveCaption;
+            gbOrientacion2.Controls.Add(cbo1Punt5);
+            gbOrientacion2.Controls.Add(cbo1Punt4);
+            gbOrientacion2.Controls.Add(cbo1Punt3);
+            gbOrientacion2.Controls.Add(cbo1Punt2);
+            gbOrientacion2.Controls.Add(cbo1Punt1);
+            gbOrientacion2.Controls.Add(lbl1DigaEnQue);
+            gbOrientacion2.Controls.Add(lbl5Hora);
+            gbOrientacion2.Controls.Add(lbl4FechaHoy);
+            gbOrientacion2.Controls.Add(lbl3Dia);
+            gbOrientacion2.Controls.Add(lbl2Mes);
+            gbOrientacion2.Controls.Add(lbl1Año);
+            gbOrientacion2.Controls.Add(lbl1);
+            gbOrientacion2.Controls.Add(lbl1Hasta5);
+            gbOrientacion2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            gbOrientacion2.Location = new Point(40, 86);
+            gbOrientacion2.Name = "gbOrientacion2";
+            gbOrientacion2.Size = new Size(713, 153);
+            gbOrientacion2.TabIndex = 1;
+            gbOrientacion2.TabStop = false;
+            // 
+            // lbl1Hasta5
+            // 
+            lbl1Hasta5.AutoSize = true;
+            lbl1Hasta5.Location = new Point(125, 70);
+            lbl1Hasta5.Name = "lbl1Hasta5";
+            lbl1Hasta5.Size = new Size(20, 17);
+            lbl1Hasta5.TabIndex = 1;
+            lbl1Hasta5.Text = "/5";
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(18, 70);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(18, 17);
+            lbl1.TabIndex = 0;
+            lbl1.Text = "1.";
+            // 
+            // lbl1Año
+            // 
+            lbl1Año.AutoSize = true;
+            lbl1Año.Location = new Point(328, 19);
+            lbl1Año.Name = "lbl1Año";
+            lbl1Año.Size = new Size(45, 17);
+            lbl1Año.TabIndex = 2;
+            lbl1Año.Text = "1. Año";
+            // 
+            // lbl2Mes
+            // 
+            lbl2Mes.AutoSize = true;
+            lbl2Mes.Location = new Point(328, 47);
+            lbl2Mes.Name = "lbl2Mes";
+            lbl2Mes.Size = new Size(47, 17);
+            lbl2Mes.TabIndex = 3;
+            lbl2Mes.Text = "2. Mes";
+            // 
+            // lbl3Dia
+            // 
+            lbl3Dia.AutoSize = true;
+            lbl3Dia.Location = new Point(328, 74);
+            lbl3Dia.Name = "lbl3Dia";
+            lbl3Dia.Size = new Size(41, 17);
+            lbl3Dia.TabIndex = 4;
+            lbl3Dia.Text = "3. Día";
+            // 
+            // lbl4FechaHoy
+            // 
+            lbl4FechaHoy.AutoSize = true;
+            lbl4FechaHoy.Location = new Point(328, 103);
+            lbl4FechaHoy.Name = "lbl4FechaHoy";
+            lbl4FechaHoy.Size = new Size(215, 17);
+            lbl4FechaHoy.TabIndex = 5;
+            lbl4FechaHoy.Text = "4. Fecha de hoy ( día de la semana)";
+            // 
+            // lbl5Hora
+            // 
+            lbl5Hora.AutoSize = true;
+            lbl5Hora.Location = new Point(328, 129);
+            lbl5Hora.Name = "lbl5Hora";
+            lbl5Hora.Size = new Size(51, 17);
+            lbl5Hora.TabIndex = 6;
+            lbl5Hora.Text = "5. Hora";
+            // 
+            // lbl1DigaEnQue
+            // 
+            lbl1DigaEnQue.AutoSize = true;
+            lbl1DigaEnQue.Location = new Point(222, 74);
+            lbl1DigaEnQue.Name = "lbl1DigaEnQue";
+            lbl1DigaEnQue.Size = new Size(79, 17);
+            lbl1DigaEnQue.TabIndex = 7;
+            lbl1DigaEnQue.Text = "Diga en qué";
+            // 
+            // cbo1Punt1
+            // 
+            cbo1Punt1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo1Punt1.FormattingEnabled = true;
+            cbo1Punt1.Items.AddRange(new object[] { "0", "1" });
+            cbo1Punt1.Location = new Point(592, 11);
+            cbo1Punt1.Name = "cbo1Punt1";
+            cbo1Punt1.Size = new Size(121, 25);
+            cbo1Punt1.TabIndex = 8;
+            // 
+            // cbo1Punt2
+            // 
+            cbo1Punt2.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo1Punt2.FormattingEnabled = true;
+            cbo1Punt2.Items.AddRange(new object[] { "0", "1" });
+            cbo1Punt2.Location = new Point(592, 39);
+            cbo1Punt2.Name = "cbo1Punt2";
+            cbo1Punt2.Size = new Size(121, 25);
+            cbo1Punt2.TabIndex = 9;
+            // 
+            // cbo1Punt3
+            // 
+            cbo1Punt3.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo1Punt3.FormattingEnabled = true;
+            cbo1Punt3.Items.AddRange(new object[] { "0", "1" });
+            cbo1Punt3.Location = new Point(592, 70);
+            cbo1Punt3.Name = "cbo1Punt3";
+            cbo1Punt3.Size = new Size(121, 25);
+            cbo1Punt3.TabIndex = 10;
+            // 
+            // cbo1Punt4
+            // 
+            cbo1Punt4.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo1Punt4.FormattingEnabled = true;
+            cbo1Punt4.Items.AddRange(new object[] { "0", "1" });
+            cbo1Punt4.Location = new Point(592, 99);
+            cbo1Punt4.Name = "cbo1Punt4";
+            cbo1Punt4.Size = new Size(121, 25);
+            cbo1Punt4.TabIndex = 11;
+            // 
+            // cbo1Punt5
+            // 
+            cbo1Punt5.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo1Punt5.FormattingEnabled = true;
+            cbo1Punt5.Items.AddRange(new object[] { "0", "1" });
+            cbo1Punt5.Location = new Point(592, 128);
+            cbo1Punt5.Name = "cbo1Punt5";
+            cbo1Punt5.Size = new Size(121, 25);
+            cbo1Punt5.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.GradientInactiveCaption;
+            groupBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            groupBox1.Controls.Add(cbo2Punt5);
+            groupBox1.Controls.Add(cbo2Punt4);
+            groupBox1.Controls.Add(cbo2Punt3);
+            groupBox1.Controls.Add(cbo2Punt2);
+            groupBox1.Controls.Add(cbo2Punt1);
+            groupBox1.Controls.Add(lbl2DigaEnQue);
+            groupBox1.Controls.Add(lblPisoBarrioVereda);
+            groupBox1.Controls.Add(lblSitioLugar);
+            groupBox1.Controls.Add(lbl2Departamento);
+            groupBox1.Controls.Add(lblCiudad);
+            groupBox1.Controls.Add(lblPais);
+            groupBox1.Controls.Add(lbl2);
+            groupBox1.Controls.Add(lbl2Hasta5);
+            groupBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(40, 245);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(713, 153);
+            groupBox1.TabIndex = 13;
+            groupBox1.TabStop = false;
+            // 
+            // cbo2Punt5
+            // 
+            cbo2Punt5.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo2Punt5.FormattingEnabled = true;
+            cbo2Punt5.Items.AddRange(new object[] { "0", "1" });
+            cbo2Punt5.Location = new Point(592, 128);
+            cbo2Punt5.Name = "cbo2Punt5";
+            cbo2Punt5.Size = new Size(121, 25);
+            cbo2Punt5.TabIndex = 12;
+            // 
+            // cbo2Punt4
+            // 
+            cbo2Punt4.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo2Punt4.FormattingEnabled = true;
+            cbo2Punt4.Items.AddRange(new object[] { "0", "1" });
+            cbo2Punt4.Location = new Point(592, 99);
+            cbo2Punt4.Name = "cbo2Punt4";
+            cbo2Punt4.Size = new Size(121, 25);
+            cbo2Punt4.TabIndex = 11;
+            // 
+            // cbo2Punt3
+            // 
+            cbo2Punt3.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo2Punt3.FormattingEnabled = true;
+            cbo2Punt3.Items.AddRange(new object[] { "0", "1" });
+            cbo2Punt3.Location = new Point(592, 70);
+            cbo2Punt3.Name = "cbo2Punt3";
+            cbo2Punt3.Size = new Size(121, 25);
+            cbo2Punt3.TabIndex = 10;
+            // 
+            // cbo2Punt2
+            // 
+            cbo2Punt2.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo2Punt2.FormattingEnabled = true;
+            cbo2Punt2.Items.AddRange(new object[] { "0", "1" });
+            cbo2Punt2.Location = new Point(592, 39);
+            cbo2Punt2.Name = "cbo2Punt2";
+            cbo2Punt2.Size = new Size(121, 25);
+            cbo2Punt2.TabIndex = 9;
+            // 
+            // lbl2DigaEnQue
+            // 
+            lbl2DigaEnQue.AutoSize = true;
+            lbl2DigaEnQue.Location = new Point(222, 74);
+            lbl2DigaEnQue.Name = "lbl2DigaEnQue";
+            lbl2DigaEnQue.Size = new Size(79, 17);
+            lbl2DigaEnQue.TabIndex = 7;
+            lbl2DigaEnQue.Text = "Diga en qué";
+            // 
+            // lblPisoBarrioVereda
+            // 
+            lblPisoBarrioVereda.AutoSize = true;
+            lblPisoBarrioVereda.Location = new Point(328, 129);
+            lblPisoBarrioVereda.Name = "lblPisoBarrioVereda";
+            lblPisoBarrioVereda.Size = new Size(151, 17);
+            lblPisoBarrioVereda.TabIndex = 6;
+            lblPisoBarrioVereda.Text = "10.Piso / Barrio / Vereda";
+            // 
+            // lblSitioLugar
+            // 
+            lblSitioLugar.AutoSize = true;
+            lblSitioLugar.Location = new Point(328, 103);
+            lblSitioLugar.Name = "lblSitioLugar";
+            lblSitioLugar.Size = new Size(96, 17);
+            lblSitioLugar.TabIndex = 5;
+            lblSitioLugar.Text = "9. Sitio o Lugar";
+            // 
+            // lbl2Departamento
+            // 
+            lbl2Departamento.AutoSize = true;
+            lbl2Departamento.Location = new Point(328, 74);
+            lbl2Departamento.Name = "lbl2Departamento";
+            lbl2Departamento.Size = new Size(106, 17);
+            lbl2Departamento.TabIndex = 4;
+            lbl2Departamento.Text = "8. Departamento";
+            // 
+            // lblCiudad
+            // 
+            lblCiudad.AutoSize = true;
+            lblCiudad.Location = new Point(328, 47);
+            lblCiudad.Name = "lblCiudad";
+            lblCiudad.Size = new Size(63, 17);
+            lblCiudad.TabIndex = 3;
+            lblCiudad.Text = "7. Ciudad";
+            // 
+            // lblPais
+            // 
+            lblPais.AutoSize = true;
+            lblPais.Location = new Point(328, 19);
+            lblPais.Name = "lblPais";
+            lblPais.Size = new Size(45, 17);
+            lblPais.TabIndex = 2;
+            lblPais.Text = "6. Pais";
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Location = new Point(18, 70);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(18, 17);
+            lbl2.TabIndex = 0;
+            lbl2.Text = "2.";
+            // 
+            // lbl2Hasta5
+            // 
+            lbl2Hasta5.AutoSize = true;
+            lbl2Hasta5.Location = new Point(125, 70);
+            lbl2Hasta5.Name = "lbl2Hasta5";
+            lbl2Hasta5.Size = new Size(20, 17);
+            lbl2Hasta5.TabIndex = 1;
+            lbl2Hasta5.Text = "/5";
+            // 
+            // cbo2Punt1
+            // 
+            cbo2Punt1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbo2Punt1.FormattingEnabled = true;
+            cbo2Punt1.Items.AddRange(new object[] { "0", "1" });
+            cbo2Punt1.Location = new Point(592, 11);
+            cbo2Punt1.Name = "cbo2Punt1";
+            cbo2Punt1.Size = new Size(121, 25);
+            cbo2Punt1.TabIndex = 8;
+            // 
+            // btnOrientaCancelar
+            // 
+            btnOrientaCancelar.BackColor = SystemColors.ButtonFace;
+            btnOrientaCancelar.Location = new Point(291, 415);
+            btnOrientaCancelar.Name = "btnOrientaCancelar";
+            btnOrientaCancelar.Size = new Size(75, 23);
+            btnOrientaCancelar.TabIndex = 14;
+            btnOrientaCancelar.Text = "&Cancelar";
+            btnOrientaCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnOrientaGuardar
+            // 
+            btnOrientaGuardar.BackColor = SystemColors.ButtonFace;
+            btnOrientaGuardar.Location = new Point(403, 415);
+            btnOrientaGuardar.Name = "btnOrientaGuardar";
+            btnOrientaGuardar.Size = new Size(75, 23);
+            btnOrientaGuardar.TabIndex = 15;
+            btnOrientaGuardar.Text = "&Guardar";
+            btnOrientaGuardar.UseVisualStyleBackColor = false;
+            // 
+            // Orientacion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnOrientaGuardar);
+            Controls.Add(btnOrientaCancelar);
+            Controls.Add(groupBox1);
+            Controls.Add(gbOrientacion2);
+            Controls.Add(gbOrientacion);
+            Name = "Orientacion";
+            Text = "ORIENTACIÓN";
+            gbOrientacion.ResumeLayout(false);
+            gbOrientacion.PerformLayout();
+            gbOrientacion2.ResumeLayout(false);
+            gbOrientacion2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox gbOrientacion;
+        private Label lblNro;
+        private Label lblPuntajeAcumulado;
+        private GroupBox gbOrientacion2;
+        private Label lblPuntuación;
+        private Label lblOrientacion;
+        private Label lbl1Hasta5;
+        private Label lbl5Hora;
+        private Label lbl4FechaHoy;
+        private Label lbl3Dia;
+        private Label lbl2Mes;
+        private Label lbl1Año;
+        private Label lbl1;
+        private ComboBox cbo1Punt1;
+        private Label lbl1DigaEnQue;
+        private ComboBox cbo1Punt5;
+        private ComboBox cbo1Punt4;
+        private ComboBox cbo1Punt3;
+        private ComboBox cbo1Punt2;
+        private GroupBox groupBox1;
+        private ComboBox cbo2Punt5;
+        private ComboBox cbo2Punt4;
+        private ComboBox cbo2Punt3;
+        private ComboBox cbo2Punt2;
+        private Label lbl2DigaEnQue;
+        private Label lblPisoBarrioVereda;
+        private Label lblSitioLugar;
+        private Label lbl2Departamento;
+        private Label lblCiudad;
+        private Label lblPais;
+        private Label lbl2;
+        private Label lbl2Hasta5;
+        private ComboBox cbo2Punt1;
+        private Button btnOrientaCancelar;
+        private Button btnOrientaGuardar;
+    }
+}
